c5f53b7fbef0b74d267e2a9dc9b5a3c890c261e0
GitHub <rakshitbharat@gmail.com>
2024-11-29 13:59:12 +0530

    -

---
+++ sentry.config.js
@@ -0,0 +1,11 @@
+export default {
+        dsn: '',
+        tracesSampleRate: 0,
+        enabled: false,
+        telemetry: false,
+        environment: 'development',
+        release: 'dev',
+        org: 'disabled',
+        project: 'disabled',
+        authToken: 'disabled'
+    };
